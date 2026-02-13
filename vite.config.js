ff7c02adacff1c3fbd5695ac23a9fbeb1dc827ac
AdaHJ123 <otogihj@outlook.com>
2026-02-13 00:54:13 -0800

    tab icon why not fixed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/my-portfolio/' : '/',
+export default defineConfig({
+  base: './',
   plugins: [vue()],
-}))
+})
